1ee4d1b4e2cf447e6f812f4cc32d80a76ba42eda
sandh1234 <merescholar1@gmail.com>
2021-11-01 14:52:11 -0700

    third commit

---
+++ src/components/base.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header/>
+    <router-view :key="$route.path"/>
+    <Footer/>
+  </div>
+</template>
+<script>
+
+import Header from './header';
+import Footer from './footer';
+export default {
+  name: 'Base',
+  data: () => ({
+    mathGameSelector: false,
+    puzzleGameSelector: true,
+  }),
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+</style>
